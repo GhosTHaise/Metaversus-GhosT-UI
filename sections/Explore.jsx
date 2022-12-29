5aefa90997b38ec0c90ef4c502979e1ebd565035
GhosTHaise <Ghostrex2@gmail.com>
2022-12-29 21:06:48 +0300

    add some gradients and build explore section

--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -1,8 +1,29 @@
 'use client';
 
+import { useState } from "react";
+import {motion} from "framer-motion"
+import {ExploreCard,TitleText,TypingText} from "../components"
+
+import styles from "../styles";
+import { fadeIn,staggerContainer } from "../utils/motion";
+
+import { exploreWorlds } from "../constants";
+
 const Explore = () => (
-  <section>
-    Explore section
+  <section className={`${styles.paddings}`} id="explore">
+    <motion.div 
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{once : false , amount : 0.25}}
+      className={`${styles.innerWidth} mx-auto flex flex-col`}
+    >
+        <TypingText title="| The World" textStyles="text-center" />
+        <TitleText title={
+            <>Choose the world you want 
+            <br className="md:block hidden" /> to explore
+        </>} textStyles="text-center" />
+    </motion.div>
   </section>
 );
 
