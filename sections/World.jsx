629ceb4e40c6d51c1e314fcc4c7d9391e9aca74b
GhosTHaise <Ghostrex2@gmail.com>
2023-01-04 17:28:52 +0300

    add person to map area

--- sections/World.jsx
+++ sections/World.jsx
@@ -25,6 +25,21 @@ const World = () => (
           className={`relative mt-[68px] flex w-full h0-[550px]`}
         >
             <img src="./map.png" alt="map" className="w-full h-full object-cover" />
+            <div className="absolute bottom-20 right-20 w-[70px] z-[70px] p-[6px] rounded-full
+                     bg-[#5d6680]
+            ">
+                  <img src="people-01.png" alt="people1" className="w-full h-full" />
+            </div>
+            <div className="absolute top-10 left-20 w-[70px] z-[70px] p-[6px] rounded-full
+                     bg-[#5d6680]
+            ">
+                  <img src="people-02.png" alt="people2" className="w-full h-full" />
+            </div>
+            <div className="absolute top-1/2 left-[45%] w-[70px] z-[70px] p-[6px] rounded-full
+                     bg-[#5d6680]
+            ">
+                  <img src="people-03.png" alt="people3" className="w-full h-full" />
+            </div>
         </motion.div>                  
      </motion.div>
   </section>
