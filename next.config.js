4190a3c9c3735e0a3ab1c9a5e6c1b55755497cb2
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-02-02 23:33:06 +0300

    fix : new workflow

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  "reactStrictMode": true,
+
+ // Add basePath
+  "basePath": '/',
   "output": "export",
   "images": {
     "unoptimized": true
