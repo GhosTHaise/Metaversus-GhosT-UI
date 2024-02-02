d2cdb31a6e24aaf4e1f4df8e628df9a02a9db0f7
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-02-02 23:16:08 +0300

    fix import

--- src/pages/page.js
+++ src/pages/page.js
@@ -1,26 +1,26 @@
-import { Footer, Navbar } from '../../components';
-import { About, Explore, Feedback, GetStarted, Hero, Insights, WhatsNew, World } from '../../sections';
+import { Footer, Navbar } from "../../components";
+import { About, Explore, Feedback, GetStarted, Hero, Insights, WhatsNew, World } from "../../sections";
 
 const Page = () => (
-  <div className='bg-primary-black overflow-hidden'>
+  <div className="bg-primary-black overflow-hidden">
     <Navbar />
     <Hero />
-    <div className='relative'>
+    <div className="relative">
         <About />
-            <div className='gradient-03 z-0' />
+            <div className="gradient-03 z-0" />
         <Explore />
     </div>
-    <div className='relative'>
+    <div className="relative">
         <GetStarted />
-            <div className='gradient-03 z-0' />
+            <div className="gradient-03 z-0" />
         <WhatsNew />
     </div>
     
     <World />
 
-    <div className='relative'>
+    <div className="relative">
           <Insights />
-              <div className='gradient-03 z-0' />
+              <div className="gradient-03 z-0" />
           <Feedback />
     </div>
     <Footer />
