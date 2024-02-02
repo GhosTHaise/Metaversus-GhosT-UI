3f1f112c753e06baddfac48c88e29bc173a9048b
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-02-02 23:12:57 +0300

    fix import

--- src/pages/page.js
+++ src/pages/page.js
@@ -1,5 +1,5 @@
-import { Footer, Navbar } from '../components';
-import { About, Explore, Feedback, GetStarted, Hero, Insights, WhatsNew, World } from '../sections';
+import { Footer, Navbar } from '../../components';
+import { About, Explore, Feedback, GetStarted, Hero, Insights, WhatsNew, World } from '../../sections';
 
 const Page = () => (
   <div className='bg-primary-black overflow-hidden'>
